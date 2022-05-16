a99b214e0f672cc0866ef51fe3f86efe28c5c774
linx3273 <ayush96.gupta@gmail.com>
2022-05-16 23:00:00 +0530

    list of apps now displayed in sorted order by name

--- app/src/main/java/com/linx/applock/applist.java
+++ app/src/main/java/com/linx/applock/applist.java
@@ -14,6 +14,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 
@@ -59,6 +61,7 @@ public class applist extends Fragment {
         intent.addCategory(Intent.CATEGORY_LAUNCHER);
 
         appslist = packageManager.queryIntentActivities(intent, 0);
+        appslist = sort(appslist, packageManager);
         appsCard = new ArrayList<>();
 
         for (ResolveInfo info: appslist){
@@ -73,4 +76,55 @@ public class applist extends Fragment {
         AppListAdapter listAdapter = new AppListAdapter(appsCard);
         recyclerView.setAdapter(listAdapter);
     }
+
+
+
+    private List<ResolveInfo> sort(List<ResolveInfo> appslist, PackageManager packageManager) {
+        /*
+        defined method to sort ResolveInfo based on name of app
+        comparison is done on the name of app the compare() logic of which is based on the strCmp()
+        function of C language
+         */
+
+        Collections.sort(appslist, new Comparator<ResolveInfo>() {
+            @Override
+            public int compare(ResolveInfo o1, ResolveInfo o2) {
+                /*
+                Implementation of strcmp(a,b) of C language
+                if
+                    a > b returns +ve value
+                    a < b returns -ve value
+                    a = b returns 0
+                 */
+
+                // extracting name of app from each ResolveInfo
+                String str1 = (String) o1.activityInfo.loadLabel(packageManager);
+                String str2 = (String) o2.activityInfo.loadLabel(packageManager);
+
+                // get length of each string
+                int l1 = str1.length();
+                int l2 = str2.length();
+
+                for (int i = 0; i < Math.min(l1,l2); i++){
+                    // obtain ASCII of character located at index i
+                    int ch1 = (int) str1.charAt(i);
+                    int ch2 = (int) str2.charAt(i);
+
+                    if ( ch1 != ch2){
+                        return ch1 - ch2;
+                    }
+                }
+
+                if (l1 != l2){
+                    return l1 - l2;     // when strings are equal upto lmin, used when strings are of unequal length
+                }
+                else{
+                    return 0;   // when strings are equal
+                }
+            }
+        });
+
+        return appslist;
+    }
+
 }
